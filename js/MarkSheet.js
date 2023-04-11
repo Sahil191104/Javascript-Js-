b982fd37c69d0f2b35a3cb17b152325bf640f7a4
Sahil191104 <sahilrupareliya95@gmail.com>
2023-04-11 22:54:08 +0545

    11_4_2023

---
+++ js/MarkSheet.js
@@ -0,0 +1,27 @@
+var n, k, r, e, v, sum, avg;
+
+n = (document.getElementById('aname').value);
+k = parseFloat(document.getElementById('am').value);
+r = parseFloat(document.getElementById('aj').value);
+e = parseFloat(document.getElementById('ad').value);
+v = parseFloat(document.getElementById('an').value);
+
+sum=k+r+e+v;
+avg=sum/4;
+
+var newTable = document.getElementById('TableScore');
+var row = newTable.insertRow(-1);
+var cell1 = row.insertCell(0);
+var cell2 = row.insertCell(0);
+var cell3 = row.insertCell(0);
+var cell4 = row.insertCell(0);
+
+cell4.innerHTML = n;
+cell3.innerHTML = sum;
+cell2.innerHTML = avg;
+            
+if (avg < 40) {
+    cell1.innerHTML="Fail";
+} else {
+    cell1.innerHTML="Pass";
+}
