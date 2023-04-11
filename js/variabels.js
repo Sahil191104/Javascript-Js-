308f1fbad8b3388904d48f70900cfeaea725db3e
Sahil191104 <sahilrupareliya95@gmail.com>
2023-04-11 07:54:52 +0530

    dfhgf

---
+++ js/variabels.js
@@ -0,0 +1,64 @@
+
+// var x = 10
+// console.log(x);
+
+// var x = 10;
+// var X = 20;
+// console.log(x);
+
+// var x = 10;
+// var x = 20;
+// console.log(x); // 20
+
+// var x;
+// console.log(x); // undefined
+
+// var x = 10;
+// var x;
+// console.log(x); // 10
+
+// Global Scope
+// var x = 10;
+// {
+//     // var x = 20;
+//     // Block Scope or Local Scope
+// }
+
+// console.log(x);
+
+// Let:-
+
+// let x = 10;
+// {
+//     let x = 20;
+// }
+
+// console.log(x);
+
+// let x = 10;
+// let x = 10;
+// console.log(x); // error
+
+// let x;  //declaration or defined
+// x = 10; //initilization
+// console.log(x);
+
+// let x = 10;
+// x = x + 2;
+// console.log(x); // 12
+
+// Const:-
+
+// const x = 10;
+// x = x + 2;
+// onsole.log(x); // error
+
+// const x;
+// x = x + 10;
+// console.log(x); // error
+
+const x = 10;
+{
+    const x = 20;
+}
+console.log(x);
