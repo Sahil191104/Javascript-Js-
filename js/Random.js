fe520ca5f0dce34f70fb6fbaf020677c2826d871
Sahil191104 <sahilrupareliya95@gmail.com>
2023-04-12 20:14:32 +0545

    12_4_2023

--- js/Random.js
+++ js/Random.js
@@ -1,9 +1,9 @@
+let w = parseInt(document.getElementById("demo").innerHTML = Math.floor(Math.random() * 10));
+let y = parseInt(document.getElementById("demo1").innerHTML = Math.floor(Math.random() * 10));
+
+let z = w + y;
+
 function randome() {
-    let w = parseInt(document.getElementById("demo").innerHTML = Math.floor(Math.random() * 10));
-    let y = parseInt(document.getElementById("demo1").innerHTML = Math.floor(Math.random() * 10));
-
-    let z = w + y;
-
     let x = parseInt(document.getElementById("demo3").value);
 
     console.log(x, z);
@@ -11,6 +11,8 @@ function randome() {
     if (z == x) {
         console.log("true");
     } else {
-        console.log("false");
+        alert("Wrong Answer!");
+        alert("The Corect Answer is");
+        alert(z);
     }
 }
