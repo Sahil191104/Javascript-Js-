fbc9f0fc8f9ade9a93375a634a8d66a302a48ff3
Dharmik0806 <lakanidharmik0806@gmail.com>
2023-04-12 11:05:30 +0530

    12_4

--- js/Random.js
+++ js/Random.js
@@ -1,2 +1,16 @@
-document.getElementById("demo").innerHTML = Math.floor(Math.random()*10);
-document.getElementById("demo1").innerHTML = Math.floor(Math.random()*10);
+function randome() {
+    let w = parseInt(document.getElementById("demo").innerHTML = Math.floor(Math.random() * 10));
+    let y = parseInt(document.getElementById("demo1").innerHTML = Math.floor(Math.random() * 10));
+
+    let z = w + y;
+
+    let x = parseInt(document.getElementById("demo3").value);
+
+    console.log(x, z);
+
+    if (z == x) {
+        console.log("true");
+    } else {
+        console.log("false");
+    }
+}
