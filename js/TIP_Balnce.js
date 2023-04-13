49b75930e9de9b75ac633a26f75fd24f7793b226
Sahil191104 <sahilrupareliya95@gmail.com>
2023-04-13 23:19:21 +0545

    13_4_2023

--- js/TIP_Balnce.js
+++ js/TIP_Balnce.js
@@ -1,9 +1,16 @@
-let x = parseInt(document.getElementById("num1").vlaue);
-let y = parseInt(document.getElementById("num2").vlaue);
-
-let z = x * y
-
 function tip_balence() {
+    let x = parseInt(document.getElementById("num1").value);
+    let y = parseInt(document.getElementById("num2").value);
+
+    // let x = parseInt(document.getElementById("num1").vlaue);
+    // let y = parseInt(document.getElementById("num2").vlaue);
+    
+    let z = x * y/100;
+    let q = x + z;
+
+    document.getElementById("num3").value = z;
+    document.getElementById("num4").value = q;
+
     document.getElementById("num3").disabled = true;
     document.getElementById("num4").disabled = true;
 }
