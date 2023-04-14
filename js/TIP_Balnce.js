ee34f4157c6f961c02aa0ae1b23575a969671079
Sahil191104 <sahilrupareliya95@gmail.com>
2023-04-14 10:00:39 +0530

    14_4_2023

--- js/TIP_Balnce.js
+++ js/TIP_Balnce.js
@@ -1,6 +1,6 @@
 function tip_balence() {
-    let x = parseInt(document.getElementById("num1").value);
-    let y = parseInt(document.getElementById("num2").value);
+    let x = parseFloat(document.getElementById("num1").value);
+    let y = parseFloat(document.getElementById("num2").value);
 
     // let x = parseInt(document.getElementById("num1").vlaue);
     // let y = parseInt(document.getElementById("num2").vlaue);
@@ -11,6 +11,6 @@ function tip_balence() {
     document.getElementById("num3").value = z;
     document.getElementById("num4").value = q;
 
-    document.getElementById("num3").disabled = true;
-    document.getElementById("num4").disabled = true;
+    // document.getElementById("num3").disabled = true;
+    // document.getElementById("num4").disabled = true;
 }
---
+++ js/UDF.js
@@ -0,0 +1,39 @@
+// Function declaration
+function grreting() {
+    console.log("Good Morning");
+}
+
+// Function call
+grreting();
+
+function calculateAge(dobage) {
+    let year;
+
+    year = 2023 - dobage;
+
+    return year;
+}
+
+let x = calculateAge(1992);
+console.log(x);
+
+let y = calculateAge(1998);
+console.log(y);
+
+
+
+// Arrow Function:-
+const age = (dobyear) => {
+    let Age;
+
+    Age = 2023 - dobyear;
+
+    return Age;
+}
+
+let q = age(1982);
+console.log(q);
+
+const crosting = () => {
+    
+}
