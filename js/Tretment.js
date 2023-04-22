af495c49af772b9197061442b027088eb8d912f0
Dharmik0806 <lakanidharmik0806@gmail.com>
2023-04-22 09:38:17 +0530

    22_4_2023

--- js/Tretment.js
+++ js/Tretment.js
@@ -8,28 +8,51 @@ function tretmenttable() {
     let q = document.getElementById("one2");
 
     document.getElementById("maintable").style.display = "inline-block";
-
+    document.getElementById("datetable").style.display = "inline-block";
+    document.getElementById("book").style.display = "inline-block";
     if (trementv === 'select') {
         document.getElementById("maintable").style.display = "none";
+        document.getElementById("datetable").style.display = "none";
+        document.getElementById("book").style.display = "none";
     } else if (trementv === 't') {
         o.innerHTML = "Teeth Cleanings";
         s.innerHTML = 1;
         q.innerHTML = 500;
     } else if (trementv === 'f') {
         o.innerHTML = "Filings";
-        s.innerHTML = 1;
-        q.innerHTML = 500;
+        s.innerHTML = 2;
+        q.innerHTML = 1500;
     } else if (trementv === 'r') {
         o.innerHTML = "Root Canal Treatment";
-        s.innerHTML = 1;
-        q.innerHTML = 500;
+        s.innerHTML = 4;
+        q.innerHTML = 2500;
     } else if (trementv === 'c') {
         o.innerHTML = "RCT + Cover";
-        s.innerHTML = 1;
-        q.innerHTML = 500;
+        s.innerHTML = 6;
+        q.innerHTML = 12000;
     } else if (trementv === 'b') {
         o.innerHTML = "Braces/Invisalign";
-        s.innerHTML = 1;
-        q.innerHTML = 500;
+        s.innerHTML = 12;
+        q.innerHTML = 35000;
     }
+}
+
+function myDateFormat() {
+    var dateControl = document.getElementById("datetable");
+    console.log(dateControl.value);
+    const [year, month, day] = dateControl.value.split('-');
+    const result = [day, month, year].join('-');
+    document.getElementById("two1").innerHTML = result;
+}
+
+function tretmentbutton() {
+    b = document.getElementById("one1").value;
+    for (i=1; i<=b; i++) {
+        document.getElementById("two").innerHTML= b;
+    }
+
+    document.getElementById("maintable").style.display = "none";
+    document.getElementById("datetable").style.display = "none";
+    document.getElementById("book").style.display = "none";
+    document.getElementById("maintable1").style.display = "inline-block";
 }
