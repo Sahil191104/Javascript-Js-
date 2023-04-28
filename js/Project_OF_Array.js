fe8d830bceb71a40fd70bb9c669e00bfce58b99d
Sahil191104 <sahilrupareliya95@gmail.com>
2023-04-28 10:01:50 +0530

    28_4_2023

--- js/Project_OF_Array.js
+++ js/Project_OF_Array.js
@@ -1,35 +1,35 @@
 // 1. Finding the maximum element in an array.
-// let a = 2,b = 8,c = 4,d = 11;
+let a = 2,b = 8,c = 4,d = 11;
 
-// if (a<b) {
-//     if (a<c) {
-//         if (a<d) {
-//             console.log(a);
-//         } else {
-//             console.log(d);
-//         }
-//     } else {
-//         if (c<d) {
-//             console.log(c);
-//         } else {
-//             console.log(d);
-//         }
-//     }		
-// } else {
-//     if (b<c) {
-//         if (b<d) {
-//             console.log(b);
-//         } else {
-//             console.log(d);
-//         }			
-//     } else {
-//         if (c<d) {
-//             console.log(c);
-//         } else {
-//             console.log(d);
-//         }
-//     }
-// }
+if (a<b) {
+    if (a<c) {
+        if (a<d) {
+            console.log(a);
+        } else {
+            console.log(d);
+        }
+    } else {
+        if (c<d) {
+            console.log(c);
+        } else {
+            console.log(d);
+        }
+    }		
+} else {
+    if (b<c) {
+        if (b<d) {
+            console.log(b);
+        } else {
+            console.log(d);
+        }			
+    } else {
+        if (c<d) {
+            console.log(c);
+        } else {
+            console.log(d);
+        }
+    }
+}
 
 // 2. Finding the minimum element in an array.
 // let a = 2,b = 8,c = 4,d = 11;
@@ -65,20 +65,22 @@
 // }
 
 // 3. Sorting an array in ascending order.
-// let ascend = [5,9,1,3,4,5,0];
-// console.log(ascend.sort());
+let ascend = [5,9,1,3,4,5,0];
+console.log(ascend.sort());
 
 // 4. Sorting an array in descending order.
-// let descend = ["sahil","meet","rajan","kayur"];
-// console.log(ascend.sort());
-// console.log(ascend.reverse());
+let descend = ["sahil","meet","rajan","kayur"];
+console.log(ascend.sort());
+console.log(ascend.reverse());
 
 // 5. Reversing an array.
+let re = [5,7,9,4,2,1];
+console.log(re.reverse());
 
 // 6. Finding the sum of all elements in an array.
-// let f = [1,2,3,4,5], sum = 0;
+let f = [1,2,3,4,5], sum = 0;
 
-// for (let i=0; i<f.length; i++) {
-//     sum = sum + f[i];
-// }
-// console.log(sum);
+for (let i=0; i<f.length; i++) {
+    sum = sum + f[i];
+}
+console.log(sum);
---
+++ js/Project_OF_Array_task.js
@@ -0,0 +1,130 @@
+console.log("-----------1-----------");
+
+// 1. Finding the maximum element in an array.
+let arr1 = [5, 7, 10, 18, 1, 79];
+
+const frmax = (arr1) => {
+    max = arr1[0];
+
+    for (let i = 1; i < arr1.length; i++) {
+        if (arr1[i] > max) {
+            max = arr1[i];
+        }
+    }
+
+    console.log(max);
+}
+frmax(arr1);
+
+console.log("-----------2-----------");
+
+// 2. Finding the minimum element in an array.
+let arr2 = [19, 48, 1, 0, 75, 34];
+
+const frmin = (arr2) => {
+    min = arr2[0];
+
+    for (let i = 1; i < arr2.length; i++) {
+        if (arr2[i] < min) {
+            min = arr2[i];
+        }
+    }
+
+    console.log(min);
+}
+frmin(arr2);
+
+console.log("-----------3-----------");
+
+// 3. Sorting an array in ascending order.
+let arr3 = [5,41,65,22,75,1,90];
+
+const frsort = (arr3) => {
+    arr3.sort((a,b) => a - b);
+
+    console.log(arr3);
+}
+frsort(arr3);
+
+console.log("-----------4-----------");
+
+// 4. Sorting an array in descending order.
+let arr4 = [5,41,65,22,75,1,90];
+
+const frrev = (arr4) => {
+    arr4.sort((a,b) => b - a);
+
+    console.log(arr4);
+}
+frrev(arr4);
+
+console.log("-----------5-----------");
+
+// 5. Reversing an array.
+let arr5 = [5,41,65,22,75,1,90];
+
+const rev = (arr5) => {
+    arr5.reverse();
+
+    console.log(arr4);
+}
+rev(arr5);
+
+console.log("-----------6-----------");
+
+// 6. Finding the sum of all elements in an array.
+const frsum = (arr4) => {
+    sum = 0;
+
+    arr4.map((a) => sum = sum + a);
+
+    console.log(sum);
+}
+frsum(arr4);
+
+console.log("-----------7-----------");
+
+// 7. Finding the average of all elements in an array.
+const fravg = (arr4) => {
+    sum = 0;
+
+    arr4.map((a) => sum = sum + a);
+
+    console.log(sum / arr4.length);
+}
+fravg(arr4);
+
+console.log("-----------8-----------");
+
+// 8. Checking if an array contains a specific element.
+let arr6 = [5,41,65,22,1,90];
+
+const frincludes = (arr6, a) => {
+    let ans = arr6.includes(a);
+
+    console.log(ans);
+}
+
+frincludes(arr6, 90);
+
+console.log("-----------9-----------");
+
+// 9. Removing duplicates from an array.
+let arr7 = [5,41,65,22,1,90];
+
+const frdublicat = (arr7) => {
+
+    let unique = [];
+
+    arr7.map((a) => {
+        if (!unique.includes(a)) {
+            
+        }
+    });
+
+    console.log(unique);
+}
+
+frdublicat(arr7);
+
+console.log("-----------10-----------");
